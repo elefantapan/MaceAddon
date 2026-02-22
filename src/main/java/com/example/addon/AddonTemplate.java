c014b5bf510cced26d79a3629825eee1cbbdc0b7
EldzeyTheDev <64653775+elefantapan@users.noreply.github.com>
2026-02-22 18:12:37 +0100

    Extend AddonTemplate class from MeteorAddon

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -2,10 +2,11 @@ package com.example.addon;
 
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.addons.MeteorAddon;
 
 import com.example.addon.modules.AxeMaceStun;
 
-public class AddonTemplate {
+public class AddonTemplate extends MeteorAddon {
     public static final Category CATEGORY = new Category("Combat");
 
     public void onInitialize() {
