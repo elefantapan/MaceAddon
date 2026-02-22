9ade25e82b97ed8746417d97b25119d533bd3f14
EldzeyTheDev <64653775+elefantapan@users.noreply.github.com>
2026-02-22 16:51:58 +0100

    Refactor CATEGORY to use Categories.Combat
    Updated CATEGORY to use new Categories class.

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -1,12 +1,13 @@
 package com.example.addon;
 
-import com.example.addon.modules.AxeMaceStun;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Categories;
+
+import com.example.addon.modules.AxeMaceStun;
 
 public class AddonTemplate {
-
-    public static final Category CATEGORY = Category.COMBAT;
+    public static final Category CATEGORY = Categories.Combat;
 
     public void onInitialize() {
         Modules.get().add(new AxeMaceStun());
