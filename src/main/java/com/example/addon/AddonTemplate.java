68a9be8587e9225a406768bf6ac156efbef214bf
EldzeyTheDev <64653775+elefantapan@users.noreply.github.com>
2026-02-22 16:56:33 +0100

    Replace Categories with new Category instance

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -2,12 +2,11 @@ package com.example.addon;
 
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.systems.modules.Category;
-import meteordevelopment.meteorclient.systems.modules.Categories;
 
 import com.example.addon.modules.AxeMaceStun;
 
 public class AddonTemplate {
-    public static final Category CATEGORY = Categories.Combat;
+    public static final Category CATEGORY = new Category("Combat");
 
     public void onInitialize() {
         Modules.get().add(new AxeMaceStun());
