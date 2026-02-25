adbb53148b0a402b8595c5b63ffb618967d3af42
EldzeyTheDev <64653775+elefantapan@users.noreply.github.com>
2026-02-25 18:25:11 +0100

    Update AutoWind.java

--- src/main/java/com/example/addon/modules/AutoWind.java
+++ src/main/java/com/example/addon/modules/AutoWind.java
@@ -54,13 +54,13 @@ public class AutoWind extends Module {
         // Looking down
         if (mc.player.getPitch() <= 60f)  {
             used = false
-            return
+            return;
         }
 
         // Must be on ground
         if (!mc.player.isOnGround()) {
             used = false
-            return
+            return;
         }
 
         // Optional delay
